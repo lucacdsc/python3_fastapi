6ecf96e25dd7522d94ec67d76fe824299ccd5116
Luca Clemente <clementluca6@gmail.com>
2023-09-19 20:56:46 -0300

    implementation of the post route

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import UserDb, UserPublic, UserSchema
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+database = []
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDb(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
